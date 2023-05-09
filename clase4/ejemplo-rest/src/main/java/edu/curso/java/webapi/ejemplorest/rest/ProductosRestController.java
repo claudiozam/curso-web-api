042088300fa2238568bbfaf2c7bf93cb5a15c43e
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-05-09 10:51:57 -0300

    ejemplo del primer componente para listar productos

--- clase4/ejemplo-rest/src/main/java/edu/curso/java/webapi/ejemplorest/rest/ProductosRestController.java
+++ clase4/ejemplo-rest/src/main/java/edu/curso/java/webapi/ejemplorest/rest/ProductosRestController.java
@@ -1,5 +1,6 @@
 package edu.curso.java.webapi.ejemplorest.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductosRestController {
